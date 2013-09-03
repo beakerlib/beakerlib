702523c2f36cd3647a310833850487b8e88c5921
Petr Muller <muller@redhat.com>
2013-09-03 15:01:14 +0200

    Fix a tpo in release script

--- upstream-release.sh
+++ upstream-release.sh
@@ -40,7 +40,7 @@ experimental(){
   local BRANCH="$2"
 
   checkOrDie "echo '$BRANCH' | grep -q -v master" "Testing release should not be done from master branch"
-  doOrDie "Creating an archive" "git archive --prefix=${CHECKTAG}{$BRANCH}/ -o ${CHECKTAG}${BRANCH}.tar.gz HEAD"
+  doOrDie "Creating an archive" "git archive --prefix=${CHECKTAG}${BRANCH}/ -o ${CHECKTAG}${BRANCH}.tar.gz HEAD"
 }
 
 checkTag() {
