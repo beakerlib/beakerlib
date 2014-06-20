3ea77a1e2b8af613e8dc57b8783b562a268d16d9
Dalibor Pospisil <dapospis@redhat.com>
2014-06-20 09:53:27 +0200

    use local variable instead of parameter

--- src/infrastructure.sh
+++ src/infrastructure.sh
@@ -478,10 +478,10 @@ __INTERNAL_FILEBACKUP_NAMESPACE="rlFileBackupNamespace"
 
 __INTERNAL_FILEBACKUP_SET_PATH_CLEAN() {
   local path="$1"
-  local path_encoded="$( rlHash -a hex "$1" )"
+  local path_encoded="$( rlHash -a hex "$path" )"
 
   local namespace="$2"
-  local namespace_encoded="$( rlHash -a hex "$2" )"
+  local namespace_encoded="$( rlHash -a hex "$namespace" )"
 
   rlLogDebug "rlFileBackup: Setting up the cleaning lists"
   rlLogDebug "rlFileBackup: Path [$path] Encoded [$path_encoded]"
@@ -499,7 +499,7 @@ __INTERNAL_FILEBACKUP_SET_PATH_CLEAN() {
 
 __INTERNAL_FILEBACKUP_CLEAN_PATHS() {
   local namespace="$1"
-  local namespace_encoded="$( rlHash -a hex "$1" )"
+  local namespace_encoded="$( rlHash -a hex "$namespace" )"
 
   rlLogDebug "rlFileRestore: Fetching clean-up lists for namespace: [$namespace] (encoded as [$namespace_encoded])"
 
