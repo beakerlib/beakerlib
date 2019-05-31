4c77adb9e621671c6a0de79089e18733d21a949a
Jakub Heger <jheger@redhat.com>
2019-05-31 15:29:20 +0200

    loggingTest.sh: comment out irrelevant tests
    deprecating tests deprecating rlGetArch because rlGetArch was
    undeprecated

--- src/test/loggingTest.sh
+++ src/test/loggingTest.sh
@@ -229,8 +229,9 @@ test_rlGetArch() {
   [ "$out" = 'i386' ] && out='i.8.'   # funny reg exp here
   uname -a | grep -q "$out"
   assertTrue 'rlGetArch returns correct arch' "[ $? -eq 0 ]"
-  assertTrue 'rlGetArch warns about deprecation' "rlJournalPrintText | grep 'This function is deprecated'"
-  assertTrue 'rlGetArch suggests use rlGetPrimaryArch' "rlJournalPrintText | grep 'Update test to use rlGetPrimaryArch/rlGetSecondaryArch'"
+#  rlGetArch was undeprecated
+#  assertTrue 'rlGetArch warns about deprecation' "rlJournalPrintText | grep 'This function is deprecated'"
+#  assertTrue 'rlGetArch suggests use rlGetPrimaryArch' "rlJournalPrintText | grep 'Update test to use rlGetPrimaryArch/rlGetSecondaryArch'"
 }
 
 test_rlGetDistroRelease() {
