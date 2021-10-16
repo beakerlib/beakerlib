afe05abb8d1c3021fee4676072a9b2a57653843d
Natalia Bubakova <nbubakov@redhat.com>
2021-10-17 00:06:13 +0200

    src/test/testingTest.sh: new test functions (WIP) * original tests for rlIsRHEL etc. set as callback, they were expanded with os-release approach * new test functions for rlIsOS, rlIsOSLike, rlIsOsVersion, rlIsRHELLike * WIP - early draft, not tested yet

--- src/test/testingTest.sh
+++ src/test/testingTest.sh
@@ -528,6 +528,37 @@ test_rlTestVersion() {
 EOF
 }
 
+
+# fake os-release so we can control what rlIsOS and others see
+fake_os_release(){
+if [[ ! -e "/etc/os-release.bac" ]]; then
+  mv /etc/os-release /etc/os-release.bac
+fi
+if [[ -n "$1" ]]; then
+  ## clean up after intial use of function
+  if [[ "$1" == "--clean_up" && -e "/etc/os-release.bac" ]]; then
+    rm -f "/etc/os-release"
+    mv /etc/os-release.bac /etc/os-release
+    return
+  fi
+  ## creates and fills fake os-release
+  touch /etc/os-release
+  cat > /etc/os-release <<EOF
+ID="$1"
+VERSION_ID="$3"
+EOF
+
+  if [[ $2 != "" ]]; then
+    cat >> /etc/os-release <<EOF
+ID_LIKE="$2"
+EOF
+  fi
+
+#without args, it only backups os-release so the functions cannot find it
+fi
+}
+
+
 # fake beakerlib-lsb_release so we can control what rlIsRHEL and others sees
 fake_lsb_release(){
    cat >beakerlib-lsb_release <<-EOF
@@ -546,12 +577,11 @@ EOF
     chmod a+x ./beakerlib-lsb_release
 }
 
-test_rlIsRHEL(){
-    # pretend we're RHEL6.5
-    local OLD_PATH=$PATH
-    PATH="./:"$PATH
-    fake_lsb_release "Red Hat Enterprise Linux Server" "6.5"
 
+#auxiliary function for test_rlIsRHEL, used with fake_os_release and fake_lsb_release (with arguments for distro id)
+aux_rlIsRHEL(){
+    # pretend we're RHEL 6.5
+    "$@" "6.5"
     assertTrue "major.minor detected correctly" "rlIsRHEL 6.5"
     assertTrue "major detected correctly" "rlIsRHEL 6"
     assertFalse "incorrect minor" "rlIsRHEL 6.3"
@@ -565,7 +595,8 @@ test_rlIsRHEL(){
     assertFalse "syntax error: unknown operator" "rlIsRHEL '*6.5'"
     assertTrue "syntax error: no input - checking RHEL only" "rlIsRHEL"
 
-    fake_lsb_release "Red Hat Enterprise Linux Server" "5.10"
+    # pretend we're RHEL 5.10
+    "$@" "5.10"
     assertFalse "<5" "rlIsRHEL '<5'"
     assertFalse "<5.0" "rlIsRHEL '<5.0'"
     assertFalse "<5.1" "rlIsRHEL '<5.1'"
@@ -592,54 +623,309 @@ test_rlIsRHEL(){
     assertFalse ">=5.11" "rlIsRHEL '>=5.11'"
     assertTrue "<=6" "rlIsRHEL '<=6'"
     assertTrue ">=4" "rlIsRHEL '>=4'"
+}
 
-    #clean up the fake command
+
+test_rlIsRHEL(){
+  ## with os-release
+    assertLog "approach through 'os-release'"
+    # calling test function (with first two arguments)
+    aux_rlIsRHEL fake_os_release "rhel" ""
+    # clean up
+    fake_os_release --clean_up
+
+  ## callback without os-release
+    assertLog "approach through 'lsb-release' (callback)"
+    # init
+    fake_os_release   # makes back-up of os-release
+    local OLD_PATH=$PATH
+    PATH="./:"$PATH
+    # calling test function (with first argument)
+    aux_rlIsRHEL fake_lsb_release "Red Hat Enterprise Linux Server"
+    # clean up
+    fake_os_release --clean_up
     PATH=$OLD_PATH
     rm -f "./beakerlib-lsb_release"
 }
 
-# just test that CentOS is recognized, operators are tested in rlIsRHEL
-test_rlIsCentOS(){
-    # pretend we're CentOS7.1
-    local OLD_PATH=$PATH
-    PATH="./:"$PATH
-    # yup, centos 7.1 has the build number (or what that 1503 is) in release
-    fake_lsb_release "CentOS" "7.1.1503"
 
+# axuliary function for test_rlIsCentOS
+# just test that CentOS is recognized, operators are tested in rlIsOSVersion
+aux_rlIsCentOS(){
+    # pretend we're CentOS 7.1
+    "$@" "7.1"
+    # note: in lsb-release is also etc. "7.1.1503" as the 'build number' of centos 7.1 but this would not work with function based on os-release
     assertTrue "major.minor CentOS7.1 detected correctly" "rlIsCentOS 7.1"
     assertTrue "major CentOS7 detected correctly" "rlIsCentOS 7"
-
     assertFalse "CentOS7.1 not mistaken for RHEL7.1" "rlIsRHEL 7.1"
     assertFalse "CentOS7 not mistaken for RHEL7" "rlIsRHEL 7"
 
-
-    fake_lsb_release "CentOS" "5.11"
+    # pretend we're CentOS 5.11
+    "$@" "5.11"
     assertTrue "major.minor CentOS5.11 detected correctly" "rlIsCentOS 5.11"
 
-    fake_lsb_release "CentOS" "6.6"
+    # pretend we're CentOS 6.6
+    "$@" "CentOS" "6.6"
     assertTrue "major.minor CentOS6.6 detected correctly" "rlIsCentOS 6.6"
-
-    #clean up the fake command
-    PATH=$OLD_PATH
-    rm -f "./beakerlib-lsb_release"
 }
 
-# just test that Fedora is recognized, operators are tested in rlIsRHEL
-test_rlIsFedora(){
-    # pretend we're CentOS7.1
+
+test_rlIsCentOS(){
+  ## with os-release
+    assertLog "approach through 'os-release'"
+    # calling test function (with first two arguments)
+    aux_rlIsCentOS fake_os_release "centos" ""
+    # clean up
+    fake_os_release --clean_up
+
+  ## callback without os-release
+    assertLog "approach through 'lsb-release' (callback)"
+    # init
+    fake_os_release   # makes back-up of os-release
     local OLD_PATH=$PATH
     PATH="./:"$PATH
-
-    fake_lsb_release "Fedora" "25"
-    assertTrue "major Fedora 25 detected correctly" "rlIsFedora 25"
-
-    fake_lsb_release "Fedora" "26"
-    assertTrue "major Fedora 26 detected correctly" "rlIsFedora 26"
-
-    #clean up the fake command
+    # calling test function (with first argument)
+    aux_rlIsCentOS fake_lsb_release "CentOS"
+    # clean up
+    fake_os_release --clean_up
     PATH=$OLD_PATH
     rm -f "./beakerlib-lsb_release"
 }
+
+
+# axuliary function for test_rlIsFedora
+# just test that Fedora is recognized, operators are tested in rlIsOSVersion
+aux_rlIsFedora(){
+    # pretend we're Fedora 25
+    "$@" "25"
+    assertTrue "major Fedora 25 detected correctly" "rlIsFedora 25"
+    # pretend we're Fedora 26
+    "$@" "26"
+    assertTrue "major Fedora 26 detected correctly" "rlIsFedora 26"
+}
+
+
+test_rlIsFedora(){
+  ## with os-release
+    assertLog "approach through 'os-release'"
+    # calling test function (with first two arguments)
+    aux_rlIsFedora fake_os_release "fedora" ""
+    # clean up
+    fake_os_release --clean_up
+
+  ## callback without os-release
+    assertLog "approach through 'lsb-release' (callback)"
+    # init
+    fake_os_release   # makes back-up of os-release
+    local OLD_PATH=$PATH
+    PATH="./:"$PATH
+    # calling test function (with first argument)
+    aux_rlIsFedora fake_lsb_release "Fedora"
+    # clean up
+    fake_os_release --clean_up
+    PATH=$OLD_PATH
+    rm -f "./beakerlib-lsb_release"
+}
+
+
+test_rlIsOS(){
+    # there is no os-release
+    fake_os_release
+    assertFalse "os-release does not exist" "rlIsOS rhel"
+
+    #there is wrong os-release file
+    touch /etc/os-release
+    assertFalse "there is no ID defined" "rlIsOS fedora"
+    fake_os_release "" "fedora" ""
+    assertFalse "there is no match with ID" "rlIsOS fedora"
+
+    # pretend we're RHEL 8.6 (fedora-like)
+    fake_os_release "rhel" "fedora" "8.6"
+
+    assertTrue "ID detected correctly" "rlIsOS rhel"
+    assertFalse "incorrect distro ID" "rlIsOS fedora"
+    assertFalse "no argument given" "rlIsOS"
+
+    # pretend we're Fedora 36
+    fake_os_release "fedora" "" "36"
+
+    assertTrue "ID detected correctly" "rlIsOS fedora"
+    assertFalse "incorrect ID" "rlIsOS centos"
+
+    # pretend we're CentOS Linux 8
+    fake_os_release "centos" "rhel fedora" "8"
+
+    assertTrue "ID detected correctly" "rlIsOS centos"
+    assertFalse "incorrect ID" "rlIsOS fedora"
+    assertFalse "incorrect ID" "rlIsOS rhel"
+
+    # pretend we're Oracle Linux
+    fake_os_release "ol" "fedora" "8.3"
+
+    assertTrue "ID detected correctly" "rlIsOS ol"
+    assertFalse "incorrect ID" "rlIsOS fedora"
+
+    #clean up fake os-release
+    fake_os_release --clean_up
+}
+
+
+test_rlIsOSLike(){
+    # there is no os-release
+    fake_os_release
+    assertFalse "os-release does not exist" "rlIsOSLike rhel"
+
+    #there is wrong os-release file
+    touch /etc/os-release
+    assertFalse "there is no ID_LIKE nor ID defined" "rlIsOSLike fedora"
+    fake_os_release "" "" ""
+    assertFalse "there is no match with ID_LIKE or ID" "rlIsOSLike fedora"
+
+    # pretend we're RHEL 8.6 (fedora-like)
+    fake_os_release "rhel" "fedora" "8.6"
+
+    assertTrue "ID_LIKE detected correctly" "rlIsOSLike fedora"
+    assertTrue "when wrong ID_LIKE, ID is detected correctly" "rlIsOSLike rhel"
+    assertFalse "no argument given" "rlIsOS"
+
+    # pretend we're Fedora 36
+    fake_os_release "fedora" "" "36"
+
+    assertTrue "no ID_LIKE, but ID is detected correctly" "rlIsOSLike fedora"
+    assertFalse "incorrect ID_LIKE" "rlIsOSLike centos"
+
+    # pretend we're CentOS Linux 8
+    fake_os_release "centos" "rhel fedora" "8"
+
+    assertTrue "when wrong ID_LIKE ID is detected correctly" "rlIsOSLike centos"
+    assertFalse "ID_LIKE detected correctly"  "rlIsOSLike fedora"
+    assertFalse "ID_LIKE detected correctly"  "rlIsOSLike rhel"
+    assertFalse "incorrect distro ID_LIKE" "rlIsOSLike ol"
+
+    # pretend we're Oracle Linux
+    fake_os_release "ol" "fedora" "8.3"
+
+    assertTrue "ID_LIKE detected correctly" "rlIsOSLike fedora"
+    assertTrue "when wrong ID_LIKE, ID is detected correctly" "rlIsOSLike ol"
+    assertFalse "incorrect distro ID_LIKE" "rlIsOSLike centos"
+
+    #clean up fake os-release
+    fake_os_release --clean_up
+}
+
+
+
+test_rlIsOSVersion(){
+    # there is no os-release
+    fake_os_release
+    assertFalse "os-release does not exist" "rlIsOSVersion 8.6"
+
+    #there is wrong os-release file
+    touch /etc/os-release
+    assertFalse "there is no VERSION_ID defined" "rlIsOSVersion 8.6"
+    fake_os_release "rhel" "fedora" ""
+    assertFalse "there is no match with VERSION_ID" "rlIsOSVersion 8.6"
+
+    # pretend we're RHEL 8.6 (fedora-like)
+    fake_os_release "rhel" "fedora" "8.6"
+
+    assertTrue "correct major" "rlIsOSVersion 8"
+    assertFalse "incorrect major" "rlIsOSVersion 9"
+    assertTrue "correct major.minor" "rlIsOSVersion 8.6"
+    assertFalse "incorrect minor" "rlIsOSVersion 8.5"
+    assertFalse "incorrect major" "rlIsOSVersion 7.6"
+    assertTrue "multiple majors, one correct" "rlIsOSVersion 6.6 7.6 8.6 9.6"
+    assertTrue "multiple minors, one correct" "rlIsOSVersion 8.6 8.5 8.4 8.3"
+    assertFalse "multiple args, none correct" "rlIsOSVersion 6.5 7.4 8.0"
+
+    assertTrue "<9 (escaped)" "rlIsOSVersion \<9"
+    assertFalse "<8" "rlIsOSVersion '<8'"
+    assertFalse "<8.6" "rlIsOSVersion '<8.6'"
+    assertTrue "<8.7" "rlIsOSVersion '<8.7'"
+    assertTrue ">7 (escaped)" "rlIsOSVersion \>7"
+    assertTrue ">7.7" "rlIsOSVersion '>7.7'"
+    assertFalse ">8" "rlIsOSVersion '>8'"
+    assertTrue ">8.0" "rlIsOSVersion '>8.0'"
+    assertTrue ">8.5" "rlIsOSVersion '>8.5'"
+    assertFalse ">8.6" "rlIsOSVersion '>8.6'"
+
+    assertFalse "=8" "rlIsOSVersion '=8'"
+    assertTrue "<=8" "rlIsOSVersion '<=8'"
+    assertTrue "=<8" "rlIsOSVersion '=<8'"
+    assertTrue "<=9" "rlIsOSVersion '<=9'"
+    assertFalse ">=9" "rlIsOSVersion '>=9'"
+    assertTrue ">=5" "rlIsOSVersion '>=5'"
+    assertFalse "<=8.0" "rlIsOSVersion '<=8.0'"
+    assertTrue "<=8.6" "rlIsOSVersion '<=8.6'"
+    assertTrue "<=9.5" "rlIsOSVersion '<=9.5'"
+    assertTrue ">=8" "rlIsOSVersion '>=8'"
+    assertTrue "=>8" "rlIsOSVersion '=>8'"
+    assertTrue ">=8.0" "rlIsOSVersion '>=8.0'"
+    assertTrue ">=8.5" "rlIsOSVersion '>=8.5'"
+    assertTrue "=>7.0" "rlIsOSVersion '=>7.0'"
+    assertTrue ">=7.0 (escaped)" "rlIsOSVersion \>=7.0"
+    assertTrue ">=8.6" "rlIsOSVersion '>=8.6'"
+    assertFalse ">=8.7" "rlIsOSVersion '>=8.7'"
+
+    assertTrue ">=8.6 and others" "rlIsOSVersion '>=8.6' =8 8.5 \<7.9"
+    assertFalse ">8.6 and others" "rlIsOSVersion '>8.6' =8 8.5 \<7.9"
+    assertTrue "<8.5 || <9.5 (within both majors and minors)" "rlIsOSVersion '<8.5' || rlIsOSVersion '<9.5'"
+    assertFalse "<8.5 || <9.5 (within minors)" "rlIsOSVersion 8 && rlIsOSVersion '<8.5' || rlIsOSVersion 9 && rlIsOSVersion '<9.5'"
+
+    assertFalse "syntax error: superfluous space #1" "rlIsOSVersion '>= 8.5'"
+    assertFalse "syntax error: superfluous space #2" "rlIsOSVersion '>=' '8.5'"
+    assertFalse "syntax error: operators only" "rlIsOSVersion '<='"
+    assertFalse "syntax error: unknown operator" "rlIsOSVersion '*8.5'"
+    assertFalse "syntax error: no arguments" "rlIsOSVersion"
+
+    # pretend we're Fedora 36
+    fake_os_release "fedora" "" "36"
+
+    assertTrue "36" "rlIsOSVersion 36"
+    assertFalse "=36(.0)" "rlIsOSVersion =36"
+    assertTrue "<36.1" "rlIsOSVersion \<36.1"
+    assertTrue ">35.7" "rlIsOSVersion \>35.7"
+
+    #clean up fake os-release
+    fake_os_release --clean_up
+}
+
+
+test_rlIsRHELLike(){
+    # there is no os-release
+    fake_os_release
+    assertFalse "os-release does not exist" "rlIsRHELLike 7"
+
+    # pretend we're RHEL 8.6 (fedora-like)
+    fake_os_release "rhel" "fedora" "8.6"
+
+    assertTrue "rhel-like distro and its version detected correctly" "rlIsRHELLike '>=8'"
+    assertFalse "rhel-like distro but wrong version" "rlIsRHELLike '<8'"
+    assertTrue "rhel-like distro detected correctly, no arg for version" "rlIsRHELLike"
+
+    # pretend we're Fedora 36
+    fake_os_release "fedora" "" "36"
+
+    assertFalse "it is not rhel-like distro nor version from the range" "rlIsRHELLike '>=8'"
+    assertFalse "it is not rhel-like distro" "rlIsRHELLike"
+
+    # pretend we're CentOS Linux 8
+    fake_os_release "centos" "rhel fedora" "8"
+
+    assertTrue "rhel-like distro and its version detected correctly" "rlIsRHELLike '>=8'"
+    assertFalse "rhel-like distro but wrong version" "rlIsRHELLike '<8'"
+    assertTrue "rhel-like distro detected correctly, no arg for version" "rlIsRHELLike"
+
+    # pretend we're Oracle Linux 8.3
+    fake_os_release "ol" "fedora" "8.3"
+
+    assertFalse "it is not rhel-like distro nor version from the range" "rlIsRHELLike '>=8'"
+    assertFalse "it is not rhel-like distro" "rlIsRHELLike"
+
+    #clean up fake os-release
+    fake_os_release --clean_up
+}
+
 
 test_rlHash(){
     local STRING="string to be hashed"
