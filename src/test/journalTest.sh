8bc1244d8b40ea507402616fd731552614199f62
Otto Sabart <osabart@redhat.com>
2024-11-06 09:38:04 +0100

    Add another test for special characters in the phase name

--- src/test/journalTest.sh
+++ src/test/journalTest.sh
@@ -365,5 +365,8 @@ test_phaseNameIn_rlReport(){
     journalReset
 
     silentIfNotDebug "rlPhaseStartTest '//some/-phase//na--me-'"
-    assertTrue "Phase name is correctly converted when passing value to rlReport in rlPhaseEnd" "rlPhaseEnd | grep '^some-phase-na-me '"
+    assertTrue "Phase name with slashes is correctly converted when passing value to rlReport in rlPhaseEnd" "rlPhaseEnd | grep '^some-phase-na-me '"
+
+    silentIfNotDebug "rlPhaseStartTest '\$so\$me<->phase?!*na--me/-'"
+    assertTrue "Phase name with special chars is correctly converted when passing value to rlReport in rlPhaseEnd" "rlPhaseEnd | grep '^so-me-phase-na-me '"
 }
