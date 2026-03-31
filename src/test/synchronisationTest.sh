beakerlib/beakerlib

commit 81929b59649a12b670ac85929a8f90eeb48aab2a

rlWaitForSocket fix - number of spaces (#67)

src/synchronisation.sh
@@ -481,7 +481,7 @@ rlWaitForSocket(){
         $remote && field=7
         local grep_opt="^$socket\s"
     fi
-    local cmd="ss -nl -$sock_type | tail -n+2 | sed -e 's/\s\{1,\}/;/g' | awk -F ';' '{print \$$field}' | grep -E $grep_opt >/dev/null"
+    local cmd="ss -nl -$sock_type | tail -n+2 | awk '{print \$$field}' | grep -E $grep_opt >/dev/null"
 
     if [[ ${close:-false} == true ]]; then
         rlLogInfo "rlWaitForSocket: Waiting max ${timeout}s for socket \`$socket' to close"

commit 58cd8e2d653b61ec96190a538f66ccbf25b8bce7

synchronisation.sh: fix rlWaitForSocket for unix sockets
This has been broken since 81929b59649a12b670ac85929a8f90eeb48aab2a as
we now use awk to "grep" only the field with the socket path which
doesn't have the trailing space.

Follow-up for 81929b59649a12b670ac85929a8f90eeb48aab2a.

src/synchronisation.sh
@@ -485,7 +485,7 @@ rlWaitForSocket(){
     else
         local sock_type="x"  # UNIX sockets
         $remote && field=7
-        local grep_opt="^$socket\s"
+        local grep_opt="^$socket\$"
     fi
     local cmd="ss -nl -$sock_type | tail -n+2 | awk '{print \$$field}' | grep -E $grep_opt >/dev/null"
 

src/test/synchronisationTest.sh
@@ -34,6 +34,26 @@ test_rlWaitForSocketPositive() {
     rm -rf $test_dir
 }
 
+test_rlWaitForSocketPositiveUnixSocket() {
+    local test_dir=$(mktemp -d /tmp/beakerlib-test-XXXXXX)
+
+    (sleep 5; /usr/bin/ncat -l -U "$test_dir/test.sock" > "$test_dir/out") &
+    local bg_pid=$!
+
+    silentIfNotDebug "rlWaitForSocket $test_dir/test.sock"
+    local ret=$?
+    assertTrue "Check if rlWaitForSocket return 0 when socket is opened" "[[ $ret -eq 0 ]]"
+
+    silentIfNotDebug "echo 'hello world' | /usr/bin/ncat -U $test_dir/test.sock"
+
+    kill -s SIGKILL $bg_pid 2>/dev/null 1>&2
+    wait $bg_pid 2>/dev/null 1>&2
+
+    assertTrue "Check if data was transferred" "grep 'hello world' $test_dir/out"
+
+    rm -rf $test_dir
+}
+
 test_rlWaitForSocketClose() {
     local test_dir=$(mktemp -d /tmp/beakerlib-test-XXXXXX)
 